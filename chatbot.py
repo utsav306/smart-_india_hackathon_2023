fdff0ecab4d26203169ee3a7290ff2661b24ea5d
Atul <code.atul01@gmail.com>
2023-09-20 23:21:21 +0530

    initial commit

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,11 @@
-from openai import *
+import openai
 
-print("module imported")
+openai.api_key = ''
+
+def get_api_response(prompt: str) -> str | None:
+    pass
+
+
+
+if "__name__" == "__main__":
+    pass
