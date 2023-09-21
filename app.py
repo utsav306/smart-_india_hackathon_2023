a2fbd89523fe25cbe56f8e3cfeec5fa03bf2e9d4
Atul <code.atul01@gmail.com>
2023-09-22 05:23:28 +0530

    updated code snippets for connectivity and added files

---
+++ app.py
@@ -0,0 +1,286 @@
+from flask import *
+from pyrebase import *
+import requests
+
+#---------------initializeing application------------------------
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+#---------------configuring firebase----------------------------
+
+
+firebaseConfig = {'apiKey': "AIzaSyAJA-UV_R0OHj06NK9LZa90pqTrNelopPc",
+  'authDomain': "gyaan-connect-b7b08.firebaseapp.com",
+ 'projectId': "gyaan-connect-b7b08",
+ 'storageBucket': "gyaan-connect-b7b08.appspot.com",
+  'messagingSenderId': "800780596390",
+  'appId': "1:800780596390:web:4c4029a1f7d91eb3333067",
+  'measurementId': "G-19P192XXE0",
+  'databaseURL': ""}
+
+
+
+firebase = initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+
+#-------------------------security key (do not change/remove this code)---------------------------------------
+
+app.secret_key = "SECRET_KEY"
+
+#-----------------------page routes-----------------------------------------
+
+#function for navbar
+def skeleton(webpage):
+    if 'user' in session:
+
+        #signed user
+    
+        user_id_token = session['user']["idToken"]
+        
+        auth.refresh(session['user']['refreshToken'])
+
+        user = auth.get_account_info(user_id_token)['users'][0]
+        
+        first_name = ""
+        
+        if "displayName" not in user:
+            first_name = "!"
+        else:
+            first_name = user['displayName'].split()[0]
+
+        return render_template(f'{webpage}.html', first_name=first_name)
+
+
+    else:
+
+        return render_template(f'{webpage}.html')
+
+
+
+
+
+@app.route('/')
+def home():
+    
+    if 'user' in session:
+
+        #signed user
+        
+        user_id_token = session['user']["idToken"]
+        auth.refresh(session['user']['refreshToken'])
+
+
+        user = auth.get_account_info(user_id_token)['users'][0]
+        first_name = ""
+        if "displayName" not in user:
+            first_name = "!"
+        else:
+            first_name = user['displayName'].split()[0]
+        return render_template('index.html', first_name=first_name)
+    
+    else:
+        #unsigned user
+        return render_template('index.html')
+
+
+
+
+@app.route('/signup', methods = ['GET','POST'])
+
+def signup():
+
+    if request.method == 'POST':
+
+        newname = request.form['newname']
+        newemail = request.form['newemail']
+        newpassword = request.form['newpassword']
+
+
+        try:
+            
+            user = auth.create_user_with_email_and_password(newemail, newpassword)
+
+            auth.update_profile(user["idToken"], display_name = newname)
+
+            
+
+            return redirect('/login')
+
+            
+        except Exception as e:
+            
+            message = json.loads(e.args[1])['error']['message']
+            
+            if message == "EMAIL_EXISTS":
+
+                
+                return render_template('login.html', signup_error = "Email already exists. Login with you registered email or register with a new one.", signup_display_error = True)
+            
+            
+            else:
+
+                return render_template('login.html', signup_error = "Something is not right. Please try again later or contact the administrator", signup_display_error = True)
+
+            
+
+    
+    else:
+
+
+        signup_error_message = "Something is not right. Please try again later or contact the administrator"
+        return render_template('login.html', signup_error = signup_error_message, signup_display_error = True)
+
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    
+    login_display_error = False
+    print("login execution")
+
+    if request.method == 'POST':
+        
+        email = request.form['email']
+        password = request.form['password']
+
+        
+        try:
+            
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = user
+            return redirect('/')
+        
+        except Exception as e:
+    
+            login_error = "Invalid email or password. Please try again."
+            return render_template('login.html', login_error = login_error, login_display_error = True)
+        
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+
+
+
+    session.pop('user', None)
+    session.clear() 
+
+
+    return redirect('/')
+
+
+
+
+@app.route('/dashboard')
+
+def dashboard():
+    
+    if 'user' in session:
+
+        #signed user    
+        
+        user_id_token = session['user']["idToken"]
+        
+        auth.refresh(session['user']['refreshToken'])
+
+        user = auth.get_account_info(user_id_token)['users'][0]
+        
+        first_name = ""
+        
+        if "displayName" not in user:
+            first_name = "!"
+        else:
+            first_name = user['displayName'].split()[0]
+
+        return render_template('dashboard.html', first_name=first_name)
+
+
+    else:
+
+        return redirect('/login')
+
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        try:
+            auth.send_password_reset_email(email)
+            message = "Success! Password reset link has been sent to your email"
+            return render_template('login.html', message=message, display_alert=True)
+        except Exception as e:
+            message = json.loads(e.args[1])['error']['message']
+            
+            if message == "EMAIL_NOT_FOUND":
+
+
+                return render_template('forgot_password.html', message = "Email does not exists. Enter your registered email id!", display_error = True)
+            
+            
+            else:
+
+                return render_template('forgot_password.html', message = "Something is not right. Please try again later or contact the administrator", display_error = True)
+    else:
+        return render_template('forgot_password.html')
+
+
+
+
+@app.route('/chatbot')
+def chatbot():
+    
+    if 'user' in session:
+
+        #signed user    
+        
+        user_id_token = session['user']["idToken"]
+        
+        auth.refresh(session['user']['refreshToken'])
+
+        user = auth.get_account_info(user_id_token)['users'][0]
+        
+        first_name = ""
+        
+        if "displayName" not in user:
+            first_name = "!"
+        else:
+            first_name = user['displayName'].split()[0]
+
+        return render_template('chatbot.html', first_name=first_name)
+
+
+    else:
+        return render_template('login.html')
+
+
+
+@app.route('/pageonwork')
+def working():
+    if 'user' in session:
+
+        #signed user    
+        
+        user_id_token = session['user']["idToken"]
+        
+        auth.refresh(session['user']['refreshToken'])
+
+        user = auth.get_account_info(user_id_token)['users'][0]
+        
+        first_name = ""
+        
+        if "displayName" not in user:
+            first_name = "!"
+        else:
+            first_name = user['displayName'].split()[0]
+
+        return render_template('working.html', first_name=first_name)
+
+
+    else:
+
+        return redirect('working.html')    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
