ee061fb57dff47a301b04dd5c39711bd0ff77d48
Atul <code.atul01@gmail.com>
2023-12-20 01:12:25 +0530

    updating files

--- MODULE TEST.py
+++ MODULE TEST.py
@@ -29,40 +29,43 @@ yt = YTStats(API_KEY)
 
 #print((get_video_ids("https://youtube.com/playlist?list=PLU6SqdYcYsfJV8Lfq4KFA0U8kGeJ2NGWV&si=zq8VtON19aU-aL6g"))[1])
 
-count = 0
-for x in (get_video_ids("https://youtube.com/playlist?list=PLU6SqdYcYsfJV8Lfq4KFA0U8kGeJ2NGWV&si=zq8VtON19aU-aL6g")[0]):
 
-    json_url = requests.get(yt.get_video_stats(x))
 
-    data = json.loads(json_url.text)
+def post_video_stats(playlist_url):
+    count = 0
+    for x in (get_video_ids(playlist_url)[0]):
+
+        json_url = requests.get(yt.get_video_stats(x))
+
+        data = json.loads(json_url.text)
  
     
     
-    #------------------------collecting all datas of youtube playlist videos------------------------
-    
-    video_url =  get_video_ids("https://youtube.com/playlist?list=PLU6SqdYcYsfJV8Lfq4KFA0U8kGeJ2NGWV&si=zq8VtON19aU-aL6g")[1][count]
+        #------------------------collecting all datas of youtube playlist videos------------------------
+        
+        video_url =  get_video_ids("https://youtube.com/playlist?list=PLU6SqdYcYsfJV8Lfq4KFA0U8kGeJ2NGWV&si=zq8VtON19aU-aL6g")[1][count]
 
-    video_id = data["items"][0]["id"]  #id of the video
+        video_id = data["items"][0]["id"]  #id of the video
 
-    title_of_video = data["items"][0]["snippet"]["title"]
+        title_of_video = data["items"][0]["snippet"]["title"]
 
-    tags_of_video = (data["items"][0]["snippet"]["tags"])
+        tags_of_video = (data["items"][0]["snippet"]["tags"])
 
-    channel_title = data["items"][0]["snippet"]["channelTitle"]
+        channel_title = data["items"][0]["snippet"]["channelTitle"]
 
-    video_thumbnail = data["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
-    
-    duration_of_video = int(data["items"][0]["contentDetails"]["duration"][2:].split('M')[0])
+        video_thumbnail = data["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+        
+        duration_of_video = int(data["items"][0]["contentDetails"]["duration"][2:].split('M')[0])
 
-    video_views = data["items"][0]["statistics"]["viewCount"]
+        video_views = data["items"][0]["statistics"]["viewCount"]
 
-    video_likes = data["items"][0]["statistics"]["likeCount"]
+        video_likes = data["items"][0]["statistics"]["likeCount"]
 
-    video_comments_count = data["items"][0]["statistics"]["commentCount"]
+        video_comments_count = data["items"][0]["statistics"]["commentCount"]
 
-    count+=1
+        count+=1
 
-    print(count, video_thumbnail,duration_of_video)
+        print(count, video_thumbnail,duration_of_video)
 
     
     
